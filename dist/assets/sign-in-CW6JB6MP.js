f8e83e352e25f2dc75fa2cef2c063910b4a425da
faruksarikaya <faruksrky@gmail.com>
2024-10-29 17:52:36 +0300

    build edildi ve env değiştirildi

---
+++ dist/assets/sign-in-CW6JB6MP.js
@@ -0,0 +1,1 @@
+import{j as t,H as e,C as i}from"./index-5L9W188R.js";import{J as r}from"./jwt-update-password-view-p9sAaVBR.js";import"./form-provider-x9b_PO4l.js";import"./new-password-icon-D7-EXtze.js";const s={title:`Sign in | Jwt - ${i.appName}`};function l(){return t.jsxs(t.Fragment,{children:[t.jsx(e,{children:t.jsxs("title",{children:[" ",s.title]})}),t.jsx(r,{})]})}export{l as default};
